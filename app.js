66de3be6bb20149afae74ef202f40125b7e6016a
Joshua Bizinoto <josh.bizinoto@gmail.com>
2025-03-19 21:00:40 -0300

    testando gitignore

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ responsiveVoice.speak(texto,'Brazilian Portuguese Female', {rate: 1.2} );
 }
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número de 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 a 10');
 }
 exibirMensagemInicial();
 
